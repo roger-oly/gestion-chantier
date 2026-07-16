e994837e0be30da739e63f83c7c1a18c1dbc1007
roger-oly <oly@mail.fr>
2026-07-16 15:10:37 +0000

    Configure devcontainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "================================="
+echo " Initialisation Gestion Chantier "
+echo "================================="
+
+echo ""
+echo "Java :"
+java -version
+
+echo ""
+echo "Maven :"
+mvn -version
+
+echo ""
+echo "Node :"
+node -version
+
+echo ""
+echo "NPM :"
+npm -version
+
+echo ""
+echo "Environnement prêt."
